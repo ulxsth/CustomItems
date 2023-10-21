1df2768cfe7039e9631e28a3dd4a51468dc93ffa
yotu <114195789+ulxsth@users.noreply.github.com>
2023-10-22 06:26:13 +0900

    init

---
+++ src/main/java/com/ulxsth/customitems/CustomItems.java
@@ -0,0 +1,12 @@
+package com.ulxsth.customitems;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomItems extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+}
