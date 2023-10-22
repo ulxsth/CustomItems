30aceb2f2741c7de92da95e08f9c6cec7673d873
yotu <114195789+ulxsth@users.noreply.github.com>
2023-10-22 16:01:42 +0900

    add

---
+++ src/main/java/com/ulxsth/customitems/util/ItemConfig.java
@@ -0,0 +1,39 @@
+package com.ulxsth.police_wanted.util;
+
+import com.ulxsth.customitems.CustomItemsPlugin;
+import com.ulxsth.police_wanted.model.item.GameItem;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+
+public class ItemConfig {
+    private static final CustomItemsPlugin plugin = CustomItemsPlugin.getInstance();
+    public static final String PATH = plugin.getDataFolder().getPath() + "/item.yml";
+
+    private static YamlConfiguration config;
+
+    private ItemConfig() {}
+
+    public static void initConfig() {
+        if(!new File(PATH).exists()) {
+            plugin.saveResource("item.yml", false);
+        }
+
+        config = YamlConfiguration.loadConfiguration(new File(PATH));
+    }
+
+    public static GameItem getItemById(String id) {
+        initConfig();
+        ConfigurationSection section = config.getConfigurationSection(id);
+        if(section == null) {
+            throw new IllegalArgumentException("cant find: " + id);
+        }
+
+        String materialId = section.getString("material");
+        String name = section.getString("name");
+        int itemId = section.getInt("id");
+
+        return new GameItem(materialId, name, itemId);
+    }
+}
