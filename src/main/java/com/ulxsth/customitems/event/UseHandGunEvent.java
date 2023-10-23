37260546ae628ffec3c6e3e00de0cea6414ffd1a
yotu <114195789+ulxsth@users.noreply.github.com>
2023-10-24 00:33:53 +0900

    update: 視点の直線状にエフェクトを発生させるように

--- src/main/java/com/ulxsth/customitems/event/UseHandGunEvent.java
+++ src/main/java/com/ulxsth/customitems/event/UseHandGunEvent.java
@@ -2,21 +2,28 @@ package com.ulxsth.customitems.event;
 
 import com.ulxsth.customitems.CustomItemsPlugin;
 import com.ulxsth.customitems.util.ItemConfig;
+import org.bukkit.Location;
+import org.bukkit.Particle;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
 
 public class UseHandGunEvent implements Listener {
     private static final String ITEM_LABEL = "hand_gun";
     private static final int ITEM_ID = ItemConfig.getItemByLabel(ITEM_LABEL).getId();
     private static final CustomItemsPlugin plugin = CustomItemsPlugin.getInstance();
 
+    private static final int EFFECT_RANGE = 30;
+
     @EventHandler
     public void onRightClick(PlayerInteractEvent event) {
         Player player = event.getPlayer();
         ItemStack item = event.getItem();
+        Location location = player.getEyeLocation();
+        Vector direction = location.getDirection().normalize();
 
         if (
             item != null
@@ -24,7 +31,14 @@ public class UseHandGunEvent implements Listener {
             && item.getItemMeta().getCustomModelData() == ITEM_ID
             && event.getAction().name().contains("RIGHT_CLICK")
         ) {
-            player.launchProjectile(org.bukkit.entity.Egg.class);
+            // プレイヤーの向いている方向にエフェクトを発生させる
+            for (double i = 0; i < EFFECT_RANGE; i += 0.5) {
+                double x = direction.getX() * i;
+                double y = direction.getY() * i;
+                double z = direction.getZ() * i;
+
+                player.getWorld().spawnParticle(Particle.FLAME, location.add(x, y, z), 1);
+            }
         }
     }
 }
