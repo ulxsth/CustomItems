6d6c0830a4979b4f36e704654438d42ff53a68f9
yotu <114195789+ulxsth@users.noreply.github.com>
2023-10-24 20:35:12 +0900

    update: ハンドガン実装してみたw

--- src/main/java/com/ulxsth/customitems/event/UseHandGunEvent.java
+++ src/main/java/com/ulxsth/customitems/event/UseHandGunEvent.java
@@ -4,41 +4,64 @@ import com.ulxsth.customitems.CustomItemsPlugin;
 import com.ulxsth.customitems.util.ItemConfig;
 import org.bukkit.Location;
 import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
+
+import java.util.List;
 
 public class UseHandGunEvent implements Listener {
     private static final String ITEM_LABEL = "hand_gun";
     private static final int ITEM_ID = ItemConfig.getItemByLabel(ITEM_LABEL).getId();
     private static final CustomItemsPlugin plugin = CustomItemsPlugin.getInstance();
 
-    private static final int EFFECT_RANGE = 30;
+    private static final double DAMAGE = 3;
+    private static final int AFFECT_RANGE = 100;
 
     @EventHandler
     public void onRightClick(PlayerInteractEvent event) {
-        Player player = event.getPlayer();
-        ItemStack item = event.getItem();
-        Location location = player.getEyeLocation();
-        Vector direction = location.getDirection().normalize();
+            Player player = event.getPlayer();
+            ItemStack item = event.getItem();
 
         if (
-            item != null
-            && item.getItemMeta().hasCustomModelData()
-            && item.getItemMeta().getCustomModelData() == ITEM_ID
-            && event.getAction().name().contains("RIGHT_CLICK")
+                item != null
+                && item.getItemMeta().hasCustomModelData()
+                && item.getItemMeta().getCustomModelData() == ITEM_ID
+                && event.getAction().name().contains("RIGHT_CLICK")
         ) {
-            // プレイヤーの向いている方向にエフェクトを発生させる
-            for (double i = 0; i < EFFECT_RANGE; i += 0.5) {
-                double x = direction.getX() * i;
-                double y = direction.getY() * i;
-                double z = direction.getZ() * i;
+            Location location = player.getEyeLocation();
+            Vector direction = location.getDirection().normalize();
+            boolean isAlreadyHit = false;
 
-                player.getWorld().spawnParticle(Particle.SMOKE_LARGE, location.clone().add(x, y, z), 1);
+            for (double i = 0; i <= AFFECT_RANGE; i += 0.5) {
+                if(isAlreadyHit) {
+                    break;
+                }
+
+                Location checkLocation = location.clone().add(direction.clone().multiply(i));
+                if (!checkLocation.getBlock().getType().isAir()) {
+                    // Stop checking when a non-air block is encountered
+                    break;
+                }
+
+                for (Entity entity : checkLocation.getNearbyEntities(0.5, 0.5, 0.5)) {
+                    if (entity instanceof LivingEntity && entity != player) {
+                        ((LivingEntity) entity).damage(DAMAGE); // Apply damage
+                        isAlreadyHit = true;
+                        break;
+                    }
+                }
             }
+
+            player.getWorld().playSound(location, Sound.ENTITY_FIREWORK_ROCKET_BLAST, 1, 1); // Play sound
+            player.getWorld().spawnParticle(Particle.FLAME, location, 10, direction.getX(), direction.getY(), direction.getZ(), 0.1); // Spawn particles
         }
     }
 }
