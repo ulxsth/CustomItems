9e94956c17b641625defc6b7ca082956be5f0b6c
yotu <114195789+ulxsth@users.noreply.github.com>
2023-10-26 17:14:22 +0900

    fix: 表示名を変更できてなかったので修正

--- src/main/java/com/ulxsth/customitems/model/GameItem.java
+++ src/main/java/com/ulxsth/customitems/model/GameItem.java
@@ -1,8 +1,11 @@
 package com.ulxsth.customitems.model;
 
 import de.tr7zw.changeme.nbtapi.NBT;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class GameItem {
     private final Material material;
@@ -46,8 +49,15 @@ public class GameItem {
      */
     public ItemStack createItemStack(int amount) {
         ItemStack itemStack = new ItemStack(this.material, amount);
+
+        // 表示名を変更
+        TextComponent displayName = Component.text(this.name);
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.displayName(displayName);
+        itemStack.setItemMeta(itemMeta);
+
+        // NBTにデータを保存
         NBT.modify(itemStack, (comp) -> {
-            comp.setString("display.Name", this.name);
             comp.setInteger("id", this.id);
         });
 
